ee74ca53151a4c6ee1c0ccf8d971984bad9b7463
Umayr Shahid <umayrr@hotmail.co.uk>
2015-11-20 03:26:13 +0500

    Add sub-generator for value

---
+++ generators/value/index.js
@@ -0,0 +1,72 @@
+'use strict';
+
+var yeoman = require('yeoman-generator');
+var _ = require('lodash');
+var path = require('path');
+
+var format = require('util').format;
+var write = require('html-wiring').writeFileFromString;
+
+
+module.exports = yeoman.generators.Base.extend({
+  initializing: function () {
+    this.props = {};
+
+    this.throwError = function (message) {
+      this.log.error(message);
+      process.exit(1);
+    };
+
+    this.props.root = path.join(this.destinationRoot(), './app/src');
+
+    var raw = this.args[0];
+
+    if (raw.indexOf('.') === -1)
+      this.throwError('First argument should be module name and value name separated by a dot.');
+
+    this.props.feature = raw.split('.')[0];
+    this.props.name = _.camelCase(_.deburr(raw.split('.')[1]));
+
+    if (_.isEmpty(this.props.feature) || _.isEmpty(this.props.name))
+      this.throwError('Feature and/or value name can\'t be empty.');
+
+    this.props.value = _.capitalize(this.props.name);
+
+  },
+  writing: function () {
+
+    this.fs.copy(
+      this.templatePath('value.js'),
+      this.destinationPath(format('%s/%s/%s.value.js', this.props.root, this.props.feature, this.props.name))
+    );
+
+    var modulePath = path.join(this.props.root, format('./%s/%s.module.js', this.props.feature, this.props.feature));
+
+    if (!this.fs.exists(modulePath))
+      throw new Error('Can\'t find module file for provided feature');
+
+    var file = this.fs.read(modulePath);
+
+    var lines = file.split('\n');
+    var cursor = _.findIndex(lines, function (value) {
+      return value === 'angular';
+    });
+
+    var top = _.slice(lines, 0, cursor);
+    var focus = _.slice(lines, cursor);
+
+    cursor = _.findIndex(focus, function (value) {
+      return value === ';';
+    });
+
+    var bottom = _.slice(focus, cursor);
+    focus = _.slice(focus, 0, cursor);
+
+    focus.push(format('  .filter(\'constant\', require(\'./%s.constant.js\'))', this.props.value, this.props.name));
+
+    lines = top.concat(focus).concat(bottom);
+    file = lines.join('\n');
+
+    write(file, modulePath);
+  }
+});
