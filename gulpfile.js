c262da96fe7e9cc165714a6a3a2f4ff8b8ee869c
Umayr Shahid <umayrr@hotmail.co.uk>
2015-11-20 16:25:17 -0500

    Remove istanbul and eslint

--- gulpfile.js
+++ gulpfile.js
@@ -2,34 +2,15 @@
 
 var path = require('path');
 var gulp = require('gulp');
-var eslint = require('gulp-eslint');
-var excludeGitignore = require('gulp-exclude-gitignore');
 var mocha = require('gulp-mocha');
-var istanbul = require('gulp-istanbul');
 var nsp = require('gulp-nsp');
 var plumber = require('gulp-plumber');
-
-gulp.task('static', function () {
-  return gulp.src('**/*.js')
-    .pipe(excludeGitignore())
-    .pipe(eslint())
-    .pipe(eslint.format())
-    .pipe(eslint.failAfterError());
-});
 
 gulp.task('nsp', function (cb) {
   nsp({package: path.resolve('package.json')}, cb);
 });
 
-gulp.task('pre-test', function () {
-  return gulp.src('generators/**/*.js')
-    .pipe(istanbul({
-      includeUntested: true
-    }))
-    .pipe(istanbul.hookRequire());
-});
-
-gulp.task('test', ['pre-test'], function (cb) {
+gulp.task('test', function (cb) {
   var mochaErr;
 
   gulp.src('test/**/*.js')
@@ -38,7 +19,6 @@ gulp.task('test', ['pre-test'], function (cb) {
     .on('error', function (err) {
       mochaErr = err;
     })
-    .pipe(istanbul.writeReports())
     .on('end', function () {
       cb(mochaErr);
     });
@@ -46,4 +26,4 @@ gulp.task('test', ['pre-test'], function (cb) {
 
 gulp.task('prepublish', ['nsp']);
 
-gulp.task('default', ['static', 'test']);
+gulp.task('default', ['test']);
